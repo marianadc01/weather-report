61972221623ad78ff727f71fa2a1f1bfd9e8c46c
DolgayaMaria <dolgayam@gmail.com>
2024-01-16 14:17:20 +0000

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,4 +1,4 @@
-from project import get_city_data, get_number_of_iterations, temp_classification, prec_classification
+from project import temp_classification, prec_classification
 from project import TemperatureConverter
 
 def test_temp_classification():
@@ -16,11 +16,6 @@ def test_prec_classification():
     assert prec_classification(10.0) == "Heavy rain! 🌧️🌧️"
     assert prec_classification(None) == "Missing precipitation data"
 
-def test_get_date():
-    assert get_date(31-12-2000) == "Invalid date format. Please use YYYY-MM-DD."
-    assert get_date(12-31-2000) == "Invalid date format. Please use YYYY-MM-DD."
-    assert get_date(2000-12-32) == "Invalid date. Please try again."
-
 
 # test the functions inside the class TemperatureConverter
 converter = TemperatureConverter()
@@ -34,4 +29,14 @@ def test_C_to_F():
 def test_C_to_K():
     assert converter.celsius_to_kelvin(0) == 273.15
     assert converter.celsius_to_kelvin(-273.15) == 0 
-    assert converter.celsius_to_kelvin(100) == 373.15   
+    assert converter.celsius_to_kelvin(100) == 373.15 
+
+''' def test_get_date():
+    assert get_date(31-12-2000) == "Invalid date format. Please use YYYY-MM-DD."
+    assert get_date(12-31-2000) == "Invalid date format. Please use YYYY-MM-DD."
+    assert get_date(2000-12-32) == "Invalid date. Please try again." """ # The function in the project.py doesn't have any agruments,
+    # so it has a name get_date() with empty brackets, but when you test it, you put some dates inside the brackets, that's why there's an error
+    
+
+
+ 
